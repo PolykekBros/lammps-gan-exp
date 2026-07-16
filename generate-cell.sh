2b3a36d6e182525326462e075385cc9db1ba6593
Kirill Karasev <kir.karasyov2017@yandex.ru>
2026-07-16 14:25:12 +0300

    added potentials and script

--- generate-cell.sh
+++ generate-cell.sh
@@ -1,1 +1,1 @@
-atomsk --create wurtzite 3.189 5.185 Ga N -orthogonal-cell -rotate X -90 -duplicate 10 10 5 GaN_mplane.lmp
+atomsk --create wurtzite 3.189 5.185 Ga N -orthogonal-cell -duplicate 10 5 10 GaN_ortho.lmp
