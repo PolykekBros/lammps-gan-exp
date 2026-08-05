beefd35fe6b2f4ad2a9cfbecadc525f4365c4e0c
Denis Strizhkin <strdenis02@gmail.com>
2026-08-05 11:57:55 +0300

    feat: more neb.rs imporovements

--- neb.rs
+++ neb.rs
@@ -5,12 +5,14 @@ edition = "2024"
 [dependencies]
 anyhow = "1.0.104"
 ---
-use anyhow::{Context, Result};
+#![feature(frontmatter)]
+use anyhow::{anyhow, bail, Context, Result};
 use std::{
+    array,
     cell::LazyCell,
     ffi::OsStr,
     fs::File,
-    io::{BufWriter, BufReader, BufRead, Write},
+    io::{BufRead, BufReader, BufWriter, Write},
     iter,
     path::{Path, PathBuf},
     process::Command,
@@ -19,11 +21,16 @@ use std::{
 const N: usize = 4;
 
 const IN_FILE: LazyCell<PathBuf> = LazyCell::new(|| PathBuf::from("in.neb"));
-const DUMP_LOAD: &str = "dump.load";
-const DUMP_MINIMIZE: &str = "dump.minimize";
-const DUMP_ATOM: &str = "dump.atom";
-const DUMP_ATOM_MINIMIZE: &str = "dump.atom_minimize";
-const NEB_FINAL: &str = "final.neb";
+const DUMP_LOAD: LazyCell<PathBuf> =
+    LazyCell::new(|| PathBuf::from("dump.load"));
+const DUMP_MINIMIZE: LazyCell<PathBuf> =
+    LazyCell::new(|| PathBuf::from("dump.minimize"));
+const DUMP_ATOM: LazyCell<PathBuf> =
+    LazyCell::new(|| PathBuf::from("dump.atom"));
+const DUMP_ATOM_MINIMIZE: LazyCell<PathBuf> =
+    LazyCell::new(|| PathBuf::from("dump.atom_minimize"));
+const NEB_FINAL: LazyCell<PathBuf> =
+    LazyCell::new(|| PathBuf::from("final.neb"));
 
 const A_ID: usize = 4001;
 const A_X: f64 = 15.945;
@@ -32,7 +39,17 @@ const A_Z: f64 = 27.277;
 const A_D: f64 = 5.1835;
 
 fn get_lmp_cmd() -> Result<&'static str> {
-    Ok("lmp")
+    Command::new("lmp_mpi")
+        .arg("-h")
+        .status()
+        .map(|_| "lmp_mpi")
+        .or_else(|e| match e.kind() {
+            std::io::ErrorKind::NotFound => {
+                Command::new("lmp").arg("-h").status().map(|_| "lmp")
+            }
+            _ => Err(e),
+        })
+        .map_err(anyhow::Error::from)
 }
 
 fn exec_lmp_with_args<P, I, S>(in_file: P, args: I) -> Result<()>
@@ -62,6 +79,11 @@ fn relax_stuff(coords: [f64; 3], n: usize) -> Result<()> {
     let y = coords[1];
     let z = coords[2];
     let in_file = IN_FILE.with_added_extension(&n.to_string());
+    let dump_load = DUMP_LOAD.with_added_extension(&n.to_string());
+    let dump_minimize = DUMP_MINIMIZE.with_added_extension(&n.to_string());
+    let dump_atom = DUMP_ATOM.with_added_extension(&n.to_string());
+    let dump_atom_minimize =
+        DUMP_ATOM_MINIMIZE.with_added_extension(&n.to_string());
     let file = File::open(&in_file)
         .with_context(|| format!("file: {}", in_file.display()))?;
     let mut w = BufWriter::new(file);
@@ -72,55 +94,81 @@ fn relax_stuff(coords: [f64; 3], n: usize) -> Result<()> {
     writeln!(w, "atom_style atomic")?;
     writeln!(w, "read_data GaN_ortho_rotated.lmp")?;
     writeln!(w, "change_box all z delta 0 10")?;
-    writeln!(w, "write_dump all custom {DUMP_LOAD}.{n} id type x y z")?;
+    writeln!(
+        w,
+        "write_dump all custom {} id type x y z",
+        dump_load.display()
+    )?;
     writeln!(w, "mass 1 69.723")?;
     writeln!(w, "mass 2 14.007")?;
     writeln!(w, "pair_style meam")?;
     writeln!(w, "pair_coeff * * library.meam Ga N GaN.meam Ga N")?;
     writeln!(w, "minimize 1.0e-10 1.0e-10 10000 10000")?;
     writeln!(w, "reset_timestep 0")?;
-    writeln!(w, "write_dump all custom {DUMP_MINIMIZE}.{n} id type x y z")?;
+    writeln!(
+        w,
+        "write_dump all custom {} id type x y z",
+        dump_minimize.display()
+    )?;
     writeln!(w, "create_atoms 1 single {x} {y} {z} units box")?;
-    writeln!(w, "write_dump all custom {DUMP_ATOM}.{n} id type x y z")?;
+    writeln!(
+        w,
+        "write_dump all custom {} id type x y z",
+        dump_atom.display()
+    )?;
     writeln!(w, "minimize 1.0e-10 1.0e-10 10000 10000")?;
     writeln!(w, "reset_timestep 0")?;
     writeln!(
         w,
-        "write_dump all custom {DUMP_ATOM_MINIMIZE}.{n} id type x y z"
+        "write_dump all custom {} id type x y z",
+        dump_atom_minimize.display()
     )?;
     exec_lmp(in_file)
 }
 
 fn extract_coords<P>(dump_file: P) -> Result<[f64; 3]>
-where P: AsRef<Path> {
+where
+    P: AsRef<Path>,
+{
     let dump_file = dump_file.as_ref();
     let dump_file = File::open(&dump_file)
         .with_context(|| format!("file {}", dump_file.display()))?;
-    let reader = BufReader::new(dump_file); 
+    let reader = BufReader::new(dump_file);
     for line in reader.lines() {
-       let tokens = line?.whitespace_split();
-
+        let line = line?;
+        let tokens = line.split_whitespace().collect::<Vec<_>>();
+        let idx = 0;
+        let Some(a_id) = tokens
+            .get(idx)
+            .map(|s| str::parse::<usize>(s))
+            .transpose()?
+        else {
+            continue;
+        };
+        if a_id != A_ID {
+            continue;
+        }
+        let begin = idx + 2;
+        let end = begin + 3;
+        let mut coords = [0f64; 3];
+        for (idx, coord) in tokens
+            .get(begin..end)
+            .with_context(|| format!("columns {} trough {}", begin, end))?
+            .iter()
+            .enumerate()
+        {
+            coords[idx] = coord.parse()?;
+        }
+        return Ok(coords);
     }
-    /*
-        tokens = line.split()
-        if len(tokens) == 0:
-            continue
-        try:
-            c_a_id = int(tokens[0])
-        except ValueError:
-            continue
-        if c_a_id == a_id and len(tokens) == 5:
-            x = float(tokens[2])
-            y = float(tokens[3])
-            z = float(tokens[4])
-            return x, y, z
-    raise f"no atom with id {a_id}"
-    */
-    Ok([0.0,0.0,0.0])
+    bail!("could not find coords for atom id {A_ID}");
 }
 
 fn main() -> Result<()> {
     relax_stuff([A_X, A_Y, A_Z], 1)?;
     relax_stuff([A_X, A_Y + A_D, A_Z], 2)?;
+    let coords = extract_coords(
+        DUMP_ATOM_MINIMIZE.with_added_extension(&2.to_string()),
+    )?;
     Ok(())
 }
